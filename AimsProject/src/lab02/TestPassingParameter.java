5c3dc29ce93441fb30a66f4906ccc6c397f1891f
dinh1duc2anh3 <dinh1duc2anh3@gmail.com>
2024-11-26 12:38:46 +0700

    topic/class-members

--- AimsProject/src/lab02/TestPassingParameter.java
+++ AimsProject/src/lab02/TestPassingParameter.java
@@ -3,34 +3,31 @@ package lab02;
 public class TestPassingParameter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		DigitalVideoDisc jungleDVD  = new DigitalVideoDisc("Jungle");
 		DigitalVideoDisc cinderellaDVD  = new DigitalVideoDisc("Cinderella");
 		
-		DigitalVideoDisc[] discs = {jungleDVD , cinderellaDVD };
-		
 		swap(jungleDVD, cinderellaDVD);
 		
-		System.out.println("jungle dvd title: "+ jungleDVD.getTitle());
-		System.out.println("cinderella dvd title: "+ cinderellaDVD.getTitle());
+		System.out.println("jungle dvd title: "+ jungleDVD.getTitle()+"(id = "+jungleDVD.getId()+")");
+		System.out.println("cinderella dvd title: "+ cinderellaDVD.getTitle()+"(id = "+cinderellaDVD.getId()+")");
 		
 		changeTitle(jungleDVD, cinderellaDVD.getTitle());
 		System.out.println("jungle dvd title: "+ jungleDVD.getTitle());
 		
 	}
-	//hàm swap cũ mà ko thay đổi được giá trị
-//	public static void swap(Object o1,Object o2 ) {
-//		Object tmp = o1;
-//		o1 = o2;
-//		o2 = tmp;
-//	}
-	
-	public static void swap(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
-		String tmp = dvd1.getTitle();
-		dvd1.setTitle(dvd2.getTitle());
-		dvd2.setTitle(tmp);
+//	hàm swap cũ mà ko thay đổi được giá trị
+	public static void swap(Object o1,Object o2 ) {
+		Object tmp = o1;
+		o1 = o2;
+		o2 = tmp;
 	}
 	
+//	public static void swap(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+//		String tmp = dvd1.getTitle();
+//		dvd1.setTitle(dvd2.getTitle());
+//		dvd2.setTitle(tmp);
+//	}
+//	
 	public static void changeTitle(DigitalVideoDisc dvd , String title ) {
 		String oldTitle = dvd.getTitle();
 		dvd.setTitle(title);
